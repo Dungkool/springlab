cd3ac14ae4682d5c857dd8c98e9db96269d827e4
jhs4132@gmail.com <jhs4132@gmail.com>
2023-04-18 15:06:46 +0900

    2023.4.18

---
+++ day01/src/main/java/com/kbstar/app/App.java
@@ -0,0 +1,11 @@
+package com.kbstar.app;
+
+import com.kbstar.frame.TV;
+
+public class App {
+    public static void main(String args[]){
+        TV tv = new STV();
+        tv.turnOn();
+        tv.turnOff();
+    }
+}
